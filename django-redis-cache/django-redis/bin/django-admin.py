463d641f436ccb72d5739b5001ffbbe0bd44d282
ahmedbony <ahmed.bony@northsouth.edu>
2018-11-20 14:36:34 +0600

    caching django with redis

---
+++ django-redis-cache/django-redis/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ubuntu/Desktop/ahmedbony/code_repo/Django/django-redis-cache/django-redis/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
