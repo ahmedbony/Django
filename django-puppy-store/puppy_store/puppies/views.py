d06a788431462750de769ee69db407c910695eb7
ahmedbony <ahmed.bony@northsouth.edu>
2018-11-24 12:22:05 +0600

    Django DRF with TDD

---
+++ django-puppy-store/puppy_store/puppies/views.py
@@ -0,0 +1,36 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Puppy
+from .serializers import PuppySerializer
+
+
+@api_view(['GET', 'DELETE', 'UPDATE'])
+def get_delete_update_puppy(request, pk):
+    try:
+        puppy = Puppy.objects.get(pk=pk)
+    except Puppy.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # get Details of a single puppy
+    if request.method == 'GET':
+        serializer = PuppySerializer(puppy)
+        return Response(serializer.data)
+    # delete a single puppy
+    elif request.method == 'DELETE':
+        return Response({})
+    # update details of a single puppy
+    elif request.method == 'UPDATE':
+        return Response({})
+
+
+@api_view(['GET', 'POST'])
+def get_post_puppies(request):
+    # get all puppies
+    if request.method == 'GET':
+        puppies = Puppy.objects.all()
+        serializer = PuppySerializer(puppies, many=True)
+        return Response(serializer.data)
+    # insert a new record for a puppy
+    elif request.method == 'POST':
+        return Response({})
