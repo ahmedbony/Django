3ec755e7556cbc17387c1e5c713da1ca228baa08
ahmedbony <ahmed.bony@northsouth.edu>
2018-12-03 17:28:22 +0600

    part5:database and migrations

--- django_blog/django_project/blog/views.py
+++ django_blog/django_project/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'LolCrap',
-        'title': 'blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'LolCrap2 ',
-        'title': 'blog post 2',
-        'content': 'second post content',
-        'date_posted': 'August 27, 2018'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
